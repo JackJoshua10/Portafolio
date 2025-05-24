87e25f145e5d1b8324c71c97a4a06f03a87dba37
JackJoshua10 <jack.sivipaucar17@gmail.com>
2025-05-23 20:13:21 -0500

    cambios-trabajos

--- src/componentes/data/trabajos.jsx
+++ src/componentes/data/trabajos.jsx
@@ -12,26 +12,35 @@ const trabajos = [
 			contenido: (
 				<>
 					<p>
-						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas iste doloribus saepe voluptatem.
-						Nihil quo beatae aut. Qui nam enim blanditiis obcaecati natus aliquid, rem facilis amet dolore
-						officiis provident?
+						Desarrollé una landing page estática utilizando HTML5 y CSS3 con el objetivo de presentar información de manera clara, visualmente atractiva y centrada en el usuario. Este proyecto está enfocado en aplicar buenas prácticas de maquetación web y diseño responsivo, asegurando una experiencia fluida tanto en computadoras como en dispositivos móviles.
 					</p>
-
-					<img loading="lazy" src="./assets/trabajos/2.png" alt="" />
 
 					<p>
-						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas iste doloribus saepe voluptatem.
-						Nihil quo beatae aut. Qui nam enim blanditiis obcaecati natus aliquid, rem facilis amet dolore
-						officiis provident?
+						La estructura de la página incluye secciones clave como encabezado, presentación general del contenido, detalles relevantes y un llamado a la acción destacado. Se priorizó una jerarquía visual adecuada, el uso de etiquetas semánticas, y una paleta de colores armoniosa para reforzar la identidad visual del proyecto.
 					</p>
-
-					<img loading="lazy" src="./assets/trabajos/3.png" alt="" />
 
 					<p>
-						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas iste doloribus saepe voluptatem.
-						Nihil quo beatae aut. Qui nam enim blanditiis obcaecati natus aliquid, rem facilis amet dolore
-						officiis provident?
+						Este trabajo me permitió afianzar conocimientos sobre diseño web, trabajar con layouts adaptativos usando media queries y reforzar la organización del código CSS para mantener escalabilidad y legibilidad. Fue desarrollado como ejercicio práctico para dominar la creación de interfaces simples pero efectivas.
 					</p>
+
+					<img loading="lazy" src="./assets/trabajos/ladingpagebasica.png" alt="" />
+
+					<a href="https://jackjoshua10.github.io/LandingPageBasica/" className="boton transparente" target="_blank">
+						Ver Trabajos
+						<div className="icono">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="16"
+								height="16"
+								fill="currentColor"
+								viewBox="0 0 16 16"
+							>
+								<path d="M9.752 6.193c.599.6 1.73.437 2.528-.362s.96-1.932.362-2.531c-.599-.6-1.73-.438-2.528.361-.798.8-.96 1.933-.362 2.532" />
+								<path d="M15.811 3.312c-.363 1.534-1.334 3.626-3.64 6.218l-.24 2.408a2.56 2.56 0 0 1-.732 1.526L8.817 15.85a.51.51 0 0 1-.867-.434l.27-1.899c.04-.28-.013-.593-.131-.956a9 9 0 0 0-.249-.657l-.082-.202c-.815-.197-1.578-.662-2.191-1.277-.614-.615-1.079-1.379-1.275-2.195l-.203-.083a10 10 0 0 0-.655-.248c-.363-.119-.675-.172-.955-.132l-1.896.27A.51.51 0 0 1 .15 7.17l2.382-2.386c.41-.41.947-.67 1.524-.734h.006l2.4-.238C9.005 1.55 11.087.582 12.623.208c.89-.217 1.59-.232 2.08-.188.244.023.435.06.57.093q.1.026.16.045c.184.06.279.13.351.295l.029.073a3.5 3.5 0 0 1 .157.721c.055.485.051 1.178-.159 2.065m-4.828 7.475.04-.04-.107 1.081a1.54 1.54 0 0 1-.44.913l-1.298 1.3.054-.38c.072-.506-.034-.993-.172-1.418a9 9 0 0 0-.164-.45c.738-.065 1.462-.38 2.087-1.006M5.205 5c-.625.626-.94 1.351-1.004 2.09a9 9 0 0 0-.45-.164c-.424-.138-.91-.244-1.416-.172l-.38.054 1.3-1.3c.245-.246.566-.401.91-.44l1.08-.107zm9.406-3.961c-.38-.034-.967-.027-1.746.163-1.558.38-3.917 1.496-6.937 4.521-.62.62-.799 1.34-.687 2.051.107.676.483 1.362 1.048 1.928.564.565 1.25.941 1.924 1.049.71.112 1.429-.067 2.048-.688 3.079-3.083 4.192-5.444 4.556-6.987.183-.771.18-1.345.138-1.713a3 3 0 0 0-.045-.283 3 3 0 0 0-.3-.041Z" />
+								<path d="M7.009 12.139a7.6 7.6 0 0 1-1.804-1.352A7.6 7.6 0 0 1 3.794 8.86c-1.102.992-1.965 5.054-1.839 5.18.125.126 3.936-.896 5.054-1.902Z" />
+							</svg>
+						</div>
+					</a>
 				</>
 			),
 		},
@@ -49,10 +58,38 @@ const trabajos = [
 			contenido: (
 				<>
 					<p>
-						Página personal estilo Linktree creada con HTML y CSS puro. Permite centralizar enlaces clave como CV, redes profesionales (LinkedIn, GitHub) y portafolio. El diseño es completamente responsivo, adaptado para dispositivos móviles y de escritorio. Se priorizó una estructura limpia, navegación simple y accesibilidad para usuarios.
+						Desarrollé una página web tipo Linktree utilizando únicamente HTML5 y CSS3, con el propósito de centralizar y facilitar el acceso a mis enlaces profesionales clave, como LinkedIn, GitHub, portafolio y CV en línea.
+					</p>
+					<p>
+						Este proyecto se enfocó en aplicar principios de diseño minimalista y responsivo, permitiendo una navegación clara, rápida y adaptable a diferentes dispositivos, incluyendo smartphones. 
 					</p>
 
-					<img loading="lazy" src="./assets/trabajos/imgLinktree.png" alt="" />
+					<p>
+						Además de su funcionalidad principal, este desarrollo me permitió reforzar buenas prácticas de estructura semántica y estilización CSS moderna. La interfaz fue diseñada con una jerarquía clara de botones, usando colores y espaciados que guían la atención del usuario hacia acciones concretas.
+					</p>
+
+					<p>
+						Este proyecto representa una solución útil y reutilizable tanto a nivel personal como para cualquier persona que necesite compartir múltiples enlaces desde una sola página de forma profesional.
+					</p>
+
+					<img loading="lazy" src="./assets/trabajos/linktreepersonl.png" alt="" />
+
+					<a href="https://jackjoshua10.github.io/LinktreeJackJoshua/" className="boton transparente" target="_blank">
+						Ver Trabajos
+						<div className="icono">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="16"
+								height="16"
+								fill="currentColor"
+								viewBox="0 0 16 16"
+							>
+								<path d="M9.752 6.193c.599.6 1.73.437 2.528-.362s.96-1.932.362-2.531c-.599-.6-1.73-.438-2.528.361-.798.8-.96 1.933-.362 2.532" />
+								<path d="M15.811 3.312c-.363 1.534-1.334 3.626-3.64 6.218l-.24 2.408a2.56 2.56 0 0 1-.732 1.526L8.817 15.85a.51.51 0 0 1-.867-.434l.27-1.899c.04-.28-.013-.593-.131-.956a9 9 0 0 0-.249-.657l-.082-.202c-.815-.197-1.578-.662-2.191-1.277-.614-.615-1.079-1.379-1.275-2.195l-.203-.083a10 10 0 0 0-.655-.248c-.363-.119-.675-.172-.955-.132l-1.896.27A.51.51 0 0 1 .15 7.17l2.382-2.386c.41-.41.947-.67 1.524-.734h.006l2.4-.238C9.005 1.55 11.087.582 12.623.208c.89-.217 1.59-.232 2.08-.188.244.023.435.06.57.093q.1.026.16.045c.184.06.279.13.351.295l.029.073a3.5 3.5 0 0 1 .157.721c.055.485.051 1.178-.159 2.065m-4.828 7.475.04-.04-.107 1.081a1.54 1.54 0 0 1-.44.913l-1.298 1.3.054-.38c.072-.506-.034-.993-.172-1.418a9 9 0 0 0-.164-.45c.738-.065 1.462-.38 2.087-1.006M5.205 5c-.625.626-.94 1.351-1.004 2.09a9 9 0 0 0-.45-.164c-.424-.138-.91-.244-1.416-.172l-.38.054 1.3-1.3c.245-.246.566-.401.91-.44l1.08-.107zm9.406-3.961c-.38-.034-.967-.027-1.746.163-1.558.38-3.917 1.496-6.937 4.521-.62.62-.799 1.34-.687 2.051.107.676.483 1.362 1.048 1.928.564.565 1.25.941 1.924 1.049.71.112 1.429-.067 2.048-.688 3.079-3.083 4.192-5.444 4.556-6.987.183-.771.18-1.345.138-1.713a3 3 0 0 0-.045-.283 3 3 0 0 0-.3-.041Z" />
+								<path d="M7.009 12.139a7.6 7.6 0 0 1-1.804-1.352A7.6 7.6 0 0 1 3.794 8.86c-1.102.992-1.965 5.054-1.839 5.18.125.126 3.936-.896 5.054-1.902Z" />
+							</svg>
+						</div>
+					</a>
 				</>
 			),
 		},
@@ -107,26 +144,24 @@ const trabajos = [
 			contenido: (
 				<>
 					<p>
-						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas iste doloribus saepe voluptatem.
-						Nihil quo beatae aut. Qui nam enim blanditiis obcaecati natus aliquid, rem facilis amet dolore
-						officiis provident?
+						Diseñé y desarrollé mi portafolio web personal como una plataforma centralizada para presentar mis proyectos, habilidades, experiencia y enlaces de contacto de manera profesional. El diseño inicial fue realizado en Figma, priorizando una interfaz limpia, jerarquía visual clara y una experiencia de usuario fluida.
 					</p>
 
-					<img loading="lazy" src="./assets/trabajos/Portafolio Light.png" alt="" />
+					<img loading="lazy" src="./assets/trabajos/portafolio-white.png" alt="" />
 
 					<p>
-						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas iste doloribus saepe voluptatem.
-						Nihil quo beatae aut. Qui nam enim blanditiis obcaecati natus aliquid, rem facilis amet dolore
-						officiis provident?
+						La implementación fue realizada utilizando tecnologías modernas como HTML5, CSS3, JavaScript y React con Vite como herramienta de construcción para lograr un rendimiento óptimo. El sitio es completamente responsivo, asegurando accesibilidad y funcionalidad en distintos dispositivos y resoluciones de pantalla.
 					</p>
-
-					<img loading="lazy" src="./assets/trabajos/Portafolio Dark.png" alt="" />
 
 					<p>
-						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas iste doloribus saepe voluptatem.
-						Nihil quo beatae aut. Qui nam enim blanditiis obcaecati natus aliquid, rem facilis amet dolore
-						officiis provident?
+						La estructura del portafolio incluye secciones como “Sobre mí”, “Proyectos”, “CV”, y “Contacto”. Cada sección está cuidadosamente maquetada y organizada, con componentes reutilizables en React para facilitar su escalabilidad y mantenimiento.
 					</p>
+
+					<p>
+						Este proyecto no solo representa mi identidad profesional como desarrollador web, sino que también es una prueba práctica de mis capacidades en diseño, desarrollo, control de versiones y despliegue web.
+					</p>
+
+					<img loading="lazy" src="./assets/trabajos/portafolio-dark.png" alt="" />
 				</>
 			),
 		},
@@ -144,26 +179,45 @@ const trabajos = [
 			contenido: (
 				<>
 					<p>
-						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas iste doloribus saepe voluptatem.
-						Nihil quo beatae aut. Qui nam enim blanditiis obcaecati natus aliquid, rem facilis amet dolore
-						officiis provident?
+						Desarrollé una página publicitaria responsiva utilizando React con Vite, enfocada en la presentación clara y atractiva de un producto o servicio. El proyecto incluye un diseño moderno con secciones organizadas que permiten al usuario conocer rápidamente las características, precios, opciones de descarga y acceder a una cuenta mediante un sistema de login simulado.
 					</p>
 
-					<img loading="lazy" src="./assets/trabajos/2.png" alt="" />
+					<img loading="lazy" src="./assets/trabajos/publicitario01.png" alt="" />
 
 					<p>
-						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas iste doloribus saepe voluptatem.
-						Nihil quo beatae aut. Qui nam enim blanditiis obcaecati natus aliquid, rem facilis amet dolore
-						officiis provident?
+						La estructura general incluye: <br />
+
+						• Menú de navegación con anclas hacia secciones clave: Características, Precios, Descarga e Iniciar sesión. <br />
+
+						• Interfaz construida con componentes reutilizables en React, optimizados para mejorar el rendimiento y facilitar el mantenimiento del código. <br />
+
+						• Diseño totalmente responsivo, pensado para ofrecer una experiencia fluida tanto en escritorio como en dispositivos móviles. <br />
+
+						• Implementación de efectos visuales suaves, diseño modular con CSS moderno y manejo de rutas si aplica. <br />
 					</p>
 
-					<img loading="lazy" src="./assets/trabajos/3.png" alt="" />
+					<img loading="lazy" src="./assets/trabajos/publicidad02.png" alt="" />
 
 					<p>
-						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas iste doloribus saepe voluptatem.
-						Nihil quo beatae aut. Qui nam enim blanditiis obcaecati natus aliquid, rem facilis amet dolore
-						officiis provident?
+						Este proyecto me permitió reforzar habilidades prácticas en componentización, estructura de páginas comerciales, y la creación de interfaces escalables para productos web reales. También apliqué conocimientos de UX/UI en la disposición y navegación.
 					</p>
+
+					<a href="https://publicidad-page-cg8a.vercel.app" className="boton transparente" target="_blank">
+						Ver Trabajos
+						<div className="icono">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="16"
+								height="16"
+								fill="currentColor"
+								viewBox="0 0 16 16"
+							>
+								<path d="M9.752 6.193c.599.6 1.73.437 2.528-.362s.96-1.932.362-2.531c-.599-.6-1.73-.438-2.528.361-.798.8-.96 1.933-.362 2.532" />
+								<path d="M15.811 3.312c-.363 1.534-1.334 3.626-3.64 6.218l-.24 2.408a2.56 2.56 0 0 1-.732 1.526L8.817 15.85a.51.51 0 0 1-.867-.434l.27-1.899c.04-.28-.013-.593-.131-.956a9 9 0 0 0-.249-.657l-.082-.202c-.815-.197-1.578-.662-2.191-1.277-.614-.615-1.079-1.379-1.275-2.195l-.203-.083a10 10 0 0 0-.655-.248c-.363-.119-.675-.172-.955-.132l-1.896.27A.51.51 0 0 1 .15 7.17l2.382-2.386c.41-.41.947-.67 1.524-.734h.006l2.4-.238C9.005 1.55 11.087.582 12.623.208c.89-.217 1.59-.232 2.08-.188.244.023.435.06.57.093q.1.026.16.045c.184.06.279.13.351.295l.029.073a3.5 3.5 0 0 1 .157.721c.055.485.051 1.178-.159 2.065m-4.828 7.475.04-.04-.107 1.081a1.54 1.54 0 0 1-.44.913l-1.298 1.3.054-.38c.072-.506-.034-.993-.172-1.418a9 9 0 0 0-.164-.45c.738-.065 1.462-.38 2.087-1.006M5.205 5c-.625.626-.94 1.351-1.004 2.09a9 9 0 0 0-.45-.164c-.424-.138-.91-.244-1.416-.172l-.38.054 1.3-1.3c.245-.246.566-.401.91-.44l1.08-.107zm9.406-3.961c-.38-.034-.967-.027-1.746.163-1.558.38-3.917 1.496-6.937 4.521-.62.62-.799 1.34-.687 2.051.107.676.483 1.362 1.048 1.928.564.565 1.25.941 1.924 1.049.71.112 1.429-.067 2.048-.688 3.079-3.083 4.192-5.444 4.556-6.987.183-.771.18-1.345.138-1.713a3 3 0 0 0-.045-.283 3 3 0 0 0-.3-.041Z" />
+								<path d="M7.009 12.139a7.6 7.6 0 0 1-1.804-1.352A7.6 7.6 0 0 1 3.794 8.86c-1.102.992-1.965 5.054-1.839 5.18.125.126 3.936-.896 5.054-1.902Z" />
+							</svg>
+						</div>
+					</a>
 				</>
 			),
 		},
@@ -255,26 +309,47 @@ const trabajos = [
 			contenido: (
 				<>
 					<p>
-						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas iste doloribus saepe voluptatem.
-						Nihil quo beatae aut. Qui nam enim blanditiis obcaecati natus aliquid, rem facilis amet dolore
-						officiis provident?
+						Diseñé y desarrollé una página web para restaurante utilizando React con Vite, enfocada en brindar una experiencia informativa y visualmente atractiva a los usuarios. El sitio está estructurado como una landing page con navegación fluida y anclada, permitiendo al visitante explorar fácilmente todo el contenido desde un solo lugar.
 					</p>
 
-					<img loading="lazy" src="./assets/trabajos/2.png" alt="" />
+					<img loading="lazy" src="./assets/trabajos/pizza01.png" alt="" />
 
 					<p>
-						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas iste doloribus saepe voluptatem.
-						Nihil quo beatae aut. Qui nam enim blanditiis obcaecati natus aliquid, rem facilis amet dolore
-						officiis provident?
+						La interfaz incluye: <br />
+
+						• Navbar interactivo con enlaces hacia las secciones Menú, Sucursales, Acerca de y Contacto, que redirigen dinámicamente a cada bloque mediante anclas internas. <br />
+
+						• Una sección de menú totalmente detallada con los distintos platos ofrecidos y sus precios, organizada de manera visualmente accesible. <br />
+
+						• La sección de sucursales cuenta con una integración mediante iframe de Google Maps, mostrando la ubicación del restaurante en tiempo real para facilitar la localización física. <br />
+
+						• Se añadieron detalles interactivos y animaciones suaves que enriquecen la experiencia del usuario y refuerzan la identidad visual del restaurante. <br />
+
+						• Diseño responsive, adaptado para funcionar perfectamente en dispositivos móviles, tablets y escritorio. <br />
 					</p>
 
-					<img loading="lazy" src="./assets/trabajos/3.png" alt="" />
+					<img loading="lazy" src="./assets/trabajos/pizza02.png" alt="" />
 
 					<p>
-						Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas iste doloribus saepe voluptatem.
-						Nihil quo beatae aut. Qui nam enim blanditiis obcaecati natus aliquid, rem facilis amet dolore
-						officiis provident?
+						Este proyecto refleja la aplicación práctica de React para construir sitios web funcionales con navegación dinámica y foco en diseño adaptado a negocios reales.
 					</p>
+
+					<a href="https://page-restaurant.vercel.app" className="boton transparente" target="_blank">
+						Ver Trabajos
+						<div className="icono">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								width="16"
+								height="16"
+								fill="currentColor"
+								viewBox="0 0 16 16"
+							>
+								<path d="M9.752 6.193c.599.6 1.73.437 2.528-.362s.96-1.932.362-2.531c-.599-.6-1.73-.438-2.528.361-.798.8-.96 1.933-.362 2.532" />
+								<path d="M15.811 3.312c-.363 1.534-1.334 3.626-3.64 6.218l-.24 2.408a2.56 2.56 0 0 1-.732 1.526L8.817 15.85a.51.51 0 0 1-.867-.434l.27-1.899c.04-.28-.013-.593-.131-.956a9 9 0 0 0-.249-.657l-.082-.202c-.815-.197-1.578-.662-2.191-1.277-.614-.615-1.079-1.379-1.275-2.195l-.203-.083a10 10 0 0 0-.655-.248c-.363-.119-.675-.172-.955-.132l-1.896.27A.51.51 0 0 1 .15 7.17l2.382-2.386c.41-.41.947-.67 1.524-.734h.006l2.4-.238C9.005 1.55 11.087.582 12.623.208c.89-.217 1.59-.232 2.08-.188.244.023.435.06.57.093q.1.026.16.045c.184.06.279.13.351.295l.029.073a3.5 3.5 0 0 1 .157.721c.055.485.051 1.178-.159 2.065m-4.828 7.475.04-.04-.107 1.081a1.54 1.54 0 0 1-.44.913l-1.298 1.3.054-.38c.072-.506-.034-.993-.172-1.418a9 9 0 0 0-.164-.45c.738-.065 1.462-.38 2.087-1.006M5.205 5c-.625.626-.94 1.351-1.004 2.09a9 9 0 0 0-.45-.164c-.424-.138-.91-.244-1.416-.172l-.38.054 1.3-1.3c.245-.246.566-.401.91-.44l1.08-.107zm9.406-3.961c-.38-.034-.967-.027-1.746.163-1.558.38-3.917 1.496-6.937 4.521-.62.62-.799 1.34-.687 2.051.107.676.483 1.362 1.048 1.928.564.565 1.25.941 1.924 1.049.71.112 1.429-.067 2.048-.688 3.079-3.083 4.192-5.444 4.556-6.987.183-.771.18-1.345.138-1.713a3 3 0 0 0-.045-.283 3 3 0 0 0-.3-.041Z" />
+								<path d="M7.009 12.139a7.6 7.6 0 0 1-1.804-1.352A7.6 7.6 0 0 1 3.794 8.86c-1.102.992-1.965 5.054-1.839 5.18.125.126 3.936-.896 5.054-1.902Z" />
+							</svg>
+						</div>
+					</a>
 				</>
 			),
 		},
